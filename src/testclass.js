9fe177c9b4da45db24009e27103d979074fb439e
Chris L <bobalua@gmail.com>
2024-12-15 18:29:55 -0500

    created a test file to learn class functionality

--- src/testclass.js
+++ src/testclass.js
@@ -1,8 +1,9 @@
 // This is purely a playground for me to play around with the concepts of classes and objects
 
 
-
-//TODO add an extended class for proof of concept
+// TODO mess with static methods possibly?
+// TODO figure out how to export class for use in other programs
+// TODO mess with get and set
 
 class Animal {
     constructor (_name, _color, _sound, _dangerToHumanity) {
@@ -30,13 +31,27 @@ class Animal {
             else {
                 console.log("good to go");
             }
-    }
-     
+    }    
 }
-
+// Polymorphism could be demonstrated with this extended class
+class Cryptid extends Animal {
+    constructor (_name, _color, _sound, _dangerToHumanity, _exists) {
+        super (_name, _color, _sound, _dangerToHumanity)
+        this.exists = _exists;
+    }
+    doesThisExist() {
+        if (this.exists == true) {
+            console.log("This is a real animal");
+        } else {
+            console.log("This is not a real animal");
+        }
+    }
+}
 let dog = new Animal("Charley", "gold", "woof", false);
 
 let snake = new Animal("who cares", "danger color", "danger sound", true);
+
+let bigFoot = new Cryptid("bigfoot", "brown", "sensual bellowing", false, false)
 
 console.log("Is " + dog.name + " a danger to humanity?");
 if (dog.dangerToHumanity == true) {
@@ -46,7 +61,7 @@ if (dog.dangerToHumanity == true) {
 }
 
 console.log("Snake's name is " + snake.name);
-console.log("Snake's color is " + snake.color);
+console.log(`Snake's color is $snake.color`); // I am doing something dumb and it isn't printing the instance property
 console.log("snake's sound is " + snake.sound );
 console.log("Is " + snake.name + " a danger to humanity?")
 if (snake.dangerToHumanity == true) {
@@ -55,3 +70,5 @@ if (snake.dangerToHumanity == true) {
     console.log("nah")
 }
 snake.lieDetector();
+
+bigFoot.doesThisExist();
